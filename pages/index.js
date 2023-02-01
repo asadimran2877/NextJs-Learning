478563457b8a492ffc38176a66cd71a8d43b71d0
asadimran2877 <asadimran2877@gmail.com>
2023-02-01 23:59:24 +0500

    ProductItem Added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Layout from '@/components/Layout';
+import ProductItem from '@/components/ProductItem';
+import data from '@/utils/data';
 import React from 'react';
 
 const Home = () => {
-  return <Layout>HomePage</Layout>;
+  return (
+    <Layout>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grids-cols-5">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
